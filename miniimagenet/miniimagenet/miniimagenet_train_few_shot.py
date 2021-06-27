c98741150efbaaa601fa66e52daf958cdaf232b3
LEELLL <35720971+LEELLL@users.noreply.github.com>
2021-06-27 23:31:10 +0800

    Add files via upload

---
+++ miniimagenet/miniimagenet/miniimagenet_train_few_shot.py
@@ -0,0 +1,389 @@
+#-------------------------------------
+# Project: Learning to Compare: Relation Network for Few-Shot Learning
+# Date: 2017.9.21
+# Author: Flood Sung
+# All Rights Reserved
+#-------------------------------------
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.optim.lr_scheduler import StepLR
+import numpy as np
+import task_generator_test as tg
+import os
+import math
+import argparse
+import scipy as sp
+import scipy.stats
+from resnet_model import resnet50,resnet18
+
+from polyaxon_client import tracking
+from loguru import logger
+
+parser = argparse.ArgumentParser(description="One Shot Visual Recognition")
+parser.add_argument("-f","--feature_dim",type = int, default = 8)#64 resnet50裁剪版cut34\filter为２５６ #ｒｅｓｎｅｔ１８ 32 #resnet50 32 裁剪版layer34 裁剪版filter128#裁剪版filter64+cutlayer34
+
+parser.add_argument("-r","--relation_dim",type = int, default = 8)
+parser.add_argument("-w","--class_num",type = int, default = 5)# 类别数，即为n-way
+parser.add_argument("-s","--sample_num_per_class",type = int, default = 5)#5  # 每个类别的样本数，即为k-shot
+parser.add_argument("-b","--batch_num_per_class",type = int, default = 10)#10
+parser.add_argument("-e","--episode",type = int, default= 500000)#500000 #1000
+parser.add_argument("-t","--test_episode", type = int, default = 600)#600 #20
+parser.add_argument("-i","--test_interval", type = int, default = 1000)#5000
+
+parser.add_argument("-l","--learning_rate",type = float, default = 0.001)
+parser.add_argument("-g","--gpu",type=int, default=0)
+parser.add_argument("-u","--hidden_unit",type=int,default=10)
+parser.add_argument("--model_name_or_path", default='lilingling/fewshot_apt/models_cut50/', type=str)
+parser.add_argument("--datas_name_or_path", default='lilingling/miniimagenet2/', type=str)
+args = parser.parse_args()
+
+base_path = tracking.get_data_paths()['ceph'] #　base_path为 datauser@192.168.68.79:/atpcephdata/（固态地址）
+args.output_dir = tracking.get_outputs_path()
+logger_path = os.path.join(tracking.get_outputs_path(), 'log_runtime.log')
+logger.add(logger_path)
+logger.info('!!!!!!!!')
+
+args.model_name_or_path = os.path.join(base_path, args.model_name_or_path)
+args.datas_name_or_path = os.path.join(base_path, args.datas_name_or_path)
+
+logger.info('start model')
+logger.info('args.datas_name_or_path args.model_name_or_path'+args.datas_name_or_path + args.model_name_or_path)
+
+
+
+# Hyper Parameters
+FEATURE_DIM = args.feature_dim
+RELATION_DIM = args.relation_dim
+CLASS_NUM = args.class_num
+SAMPLE_NUM_PER_CLASS = args.sample_num_per_class
+BATCH_NUM_PER_CLASS = args.batch_num_per_class
+EPISODE = args.episode
+TEST_EPISODE = args.test_episode
+TEST_INTERVAL = args.test_interval
+LEARNING_RATE = args.learning_rate
+GPU = args.gpu
+HIDDEN_UNIT = args.hidden_unit
+
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0*np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
+    return m,h
+
+class CNNEncoder(nn.Module):
+    """docstring for ClassName"""
+    def __init__(self):
+        super(CNNEncoder, self).__init__()
+        self.layer1 = nn.Sequential(
+                        nn.Conv2d(3,64,kernel_size=3,padding=0),
+                        nn.BatchNorm2d(64, momentum=1, affine=True),
+                        nn.ReLU(),
+                        nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(
+                        nn.Conv2d(64,64,kernel_size=3,padding=0),
+                        nn.BatchNorm2d(64, momentum=1, affine=True),
+                        nn.ReLU(),
+                        nn.MaxPool2d(2))
+        self.layer3 = nn.Sequential(
+                        nn.Conv2d(64,64,kernel_size=3,padding=1),
+                        nn.BatchNorm2d(64, momentum=1, affine=True),
+                        nn.ReLU())
+        self.layer4 = nn.Sequential(
+                        nn.Conv2d(64,64,kernel_size=3,padding=1),
+                        nn.BatchNorm2d(64, momentum=1, affine=True),
+                        nn.ReLU())
+       
+
+    def forward(self,x):
+        # print(x.size()) #torch.Size([25, 3, 84, 84])
+        out = self.layer1(x)
+        # print(out.size()) #torch.Size([25, 64, 41, 41])
+        out = self.layer2(out)
+        # print(out.size()) #torch.Size([25, 64, 19, 19])
+        out = self.layer3(out)
+        # print(out.size()) #torch.Size([25, 64, 19, 19])
+
+        out = self.layer4(out)
+        # print(out.size()) #torch.Size([25, 64, 19, 19])
+        
+        # print(out.shape)  ## torch.Size([25, 64, 19, 19])
+        return out # 64
+
+
+import torchvision.models as models
+class PreTrainedResNet(nn.Module):
+  def __init__(self):
+    super(PreTrainedResNet, self).__init__()
+    
+    self.resnet18 = models.resnet18(pretrained=True)
+
+    #Set gradients to false
+    
+    for param in self.resnet18.parameters():
+        param.requires_grad = False
+    
+    num_feats = self.resnet18.fc.in_features
+    self.resnet18 = nn.Sequential(*(list(self.resnet18.children())[:-1]))
+    
+  def forward(self, x):
+    x = self.resnet18 (x)
+    # print('x:',x.shape)  # x: torch.Size([25, 512, 1, 1])   x: torch.Size([50, 512, 1, 1])
+
+    x = x.view(x.shape[0],32,4,4)
+    return x
+
+class PreTrainedResNet50(nn.Module):
+  def __init__(self):
+    super(PreTrainedResNet50, self).__init__()
+    
+    self.resnet50 = models.resnet50(pretrained=True)
+
+    #Set gradients to false
+    
+    for param in self.resnet50.parameters():
+        param.requires_grad = False
+    
+    num_feats = self.resnet50.fc.in_features
+    self.resnet50 = nn.Sequential(*(list(self.resnet50.children())[:-1]))
+    
+  def forward(self, x):
+    x = self.resnet50(x)
+    # print('x:',x.shape)  # x: torch.Size([25, 2048, 1, 1])  x: torch.Size([50, 2048, 1, 1])
+
+    x = x.view(x.shape[0],32,8,8)
+    return x
+
+class RelationNetwork(nn.Module):
+    """docstring for RelationNetwork"""
+    def __init__(self,input_size,hidden_size):
+        # print(input_size,hidden_size) #64 8(原)　　resnet50:32 8    
+        super(RelationNetwork, self).__init__()
+        self.layer1 = nn.Sequential(
+                        # nn.Conv2d(64*2,64,kernel_size=3,padding=0),
+                        # nn.Conv2d(64,32,kernel_size=3,padding=1), #resmet50 18
+                        nn.Conv2d(FEATURE_DIM*2,FEATURE_DIM,kernel_size=3,padding=1), ## 16 8 resnet裁剪layer34+128+8 8 8
+                        # nn.Conv2d(32,16,kernel_size=3,padding=1), #resmet50+cut64 #resnet50裁剪layer34+filter为256 (16,8,8)
+                        # nn.BatchNorm2d(64, momentum=1, affine=True),
+                        # nn.BatchNorm2d(32, momentum=1, affine=True),#resmet50 18
+                        # nn.BatchNorm2d(16, momentum=1, affine=True),#resmet50cut64 #resnet50裁剪layer34+filter为256 (16,8,8)
+                        nn.BatchNorm2d(FEATURE_DIM, momentum=1, affine=True),# 8 resnet裁剪layer34+128+8 8 8
+                        nn.ReLU(),
+                        nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(
+                        # nn.Conv2d(64,64,kernel_size=3,padding=0),
+                        # nn.Conv2d(32,32,kernel_size=3,padding=1),#resmet50 18
+                        nn.Conv2d(FEATURE_DIM,FEATURE_DIM,kernel_size=3,padding=1),#resmet50cut64 #resnet50裁剪layer34+filter为256 (16,8,8)
+                        # nn.Conv2d(16,16,kernel_size=3,padding=1),#resmet50cut64 #resnet50裁剪layer34+filter为256 (16,8,8)
+                        # nn.BatchNorm2d(64, momentum=1, affine=True),
+                        # nn.BatchNorm2d(32, momentum=1, affine=True),#resmet50 18
+                        nn.BatchNorm2d(FEATURE_DIM, momentum=1, affine=True),#8 resnet裁剪layer34+128+8 8 8
+                        # nn.BatchNorm2d(16, momentum=1, affine=True),#resmet50cut64 #resnet50裁剪layer34+filter为256 (16,8,8)
+                        nn.ReLU(),
+                        nn.MaxPool2d(2))
+        # self.fc1 = nn.Linear(input_size*3*3,hidden_size)
+        # self.fc1 = nn.Linear(input_size,hidden_size)#resnet18　resnet50裁剪版　resnet50filter裁剪版
+        self.fc1 = nn.Linear(input_size*4,hidden_size)#resnet50 #resnet50裁剪layer34+filter为256 (16,8,8)
+
+        self.fc2 = nn.Linear(hidden_size,1)
+
+    def forward(self,x):
+        # print('RN:', x.size()) #RN: torch.Size([125, 128, 19, 19])   # resnet50: RN: torch.Size([250, 64, 8, 8])
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0),-1)
+        out = F.relu(self.fc1(out))
+        out = F.sigmoid(self.fc2(out))
+        # print('RelationNet out size:', out.size()) # torch.Size([125, 1])
+        return out
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+        m.weight.data.normal_(0, math.sqrt(2. / n))
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.fill_(1)
+        m.bias.data.zero_()
+    elif classname.find('Linear') != -1:
+        n = m.weight.size(1)
+        m.weight.data.normal_(0, 0.01)
+        m.bias.data = torch.ones(m.bias.data.size())
+
+def main():
+    # Step 1: init data folders
+    logger.info("init data folders")
+    # init character folders for dataset construction
+    metatrain_folders,metatest_folders = tg.mini_imagenet_folders()
+
+    # Step 2: init neural networks
+    logger.info("init neural networks")
+
+    # feature_encoder = CNNEncoder()
+    # feature_encoder = PreTrainedResNet()
+    # feature_encoder = PreTrainedResNet50()
+    feature_encoder = resnet50()
+    relation_network = RelationNetwork(FEATURE_DIM,RELATION_DIM)
+
+    feature_encoder.apply(weights_init)
+    relation_network.apply(weights_init)
+
+    feature_encoder.cuda(GPU)
+    relation_network.cuda(GPU)
+
+    feature_encoder_optim = torch.optim.Adam(feature_encoder.parameters(),lr=LEARNING_RATE)
+    feature_encoder_scheduler = StepLR(feature_encoder_optim,step_size=100000,gamma=0.5)
+    relation_network_optim = torch.optim.Adam(relation_network.parameters(),lr=LEARNING_RATE)
+    relation_network_scheduler = StepLR(relation_network_optim,step_size=100000,gamma=0.5)
+
+    if os.path.exists(str(args.model_name_or_path + "/miniimagenet_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")):
+        feature_encoder.load_state_dict(torch.load(str(args.model_name_or_path + "/miniimagenet_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")))
+        logger.info("load feature encoder success")
+    if os.path.exists(str(args.model_name_or_path + "/miniimagenet_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")):
+        relation_network.load_state_dict(torch.load(str(args.model_name_or_path + "/miniimagenet_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")))
+        logger.info("load relation network success")
+
+    # Step 3: build graph
+    logger.info("Training...")
+
+    last_accuracy = 0.0
+
+    for episode in range(EPISODE):
+
+        feature_encoder_scheduler.step(episode)
+        relation_network_scheduler.step(episode)
+
+        # init dataset
+        # sample_dataloader is to obtain previous samples for compare
+        # batch_dataloader is to batch samples for training
+        task = tg.MiniImagenetTask(metatrain_folders,CLASS_NUM,SAMPLE_NUM_PER_CLASS,BATCH_NUM_PER_CLASS)
+        sample_dataloader = tg.get_mini_imagenet_data_loader(task,num_per_class=SAMPLE_NUM_PER_CLASS,split="train",shuffle=False)
+        batch_dataloader = tg.get_mini_imagenet_data_loader(task,num_per_class=BATCH_NUM_PER_CLASS,split="test",shuffle=True)
+
+        # sample datas
+        samples,sample_labels = sample_dataloader.__iter__().next() #25*3*84*84
+        batches,batch_labels = batch_dataloader.__iter__().next()
+
+        # calculate features
+        sample_features = feature_encoder(Variable(samples).cuda(GPU)) # 25*64*19*19
+        # sample_features = sample_features.view(CLASS_NUM,SAMPLE_NUM_PER_CLASS,FEATURE_DIM,19,19)
+        sample_features = sample_features.view(CLASS_NUM,SAMPLE_NUM_PER_CLASS,FEATURE_DIM,8,8) #resnet50 #resnet50裁剪layer34+filter为256 (16,8,8)
+        # sample_features = sample_features.view(CLASS_NUM,SAMPLE_NUM_PER_CLASS,FEATURE_DIM,4,4) #resnet18 resnet50裁剪版 resnet50filter裁剪版
+        sample_features = torch.sum(sample_features,1).squeeze(1)
+        batch_features = feature_encoder(Variable(batches).cuda(GPU)) # 20x64*5*5
+
+        # calculate relations
+        # each batch sample link to every samples to calculate relations
+        # to form a 100x128 matrix for relation network
+        sample_features_ext = sample_features.unsqueeze(0).repeat(BATCH_NUM_PER_CLASS*CLASS_NUM,1,1,1,1)
+        batch_features_ext = batch_features.unsqueeze(0).repeat(CLASS_NUM,1,1,1,1)
+        batch_features_ext = torch.transpose(batch_features_ext,0,1)
+        # relation_pairs = torch.cat((sample_features_ext,batch_features_ext),2).view(-1,FEATURE_DIM*2,19,19)
+        relation_pairs = torch.cat((sample_features_ext,batch_features_ext),2).view(-1,FEATURE_DIM*2,8,8) #resnet50 #resnet50裁剪layer34+filter为256 (16,8,8)
+        # relation_pairs = torch.cat((sample_features_ext,batch_features_ext),2).view(-1,FEATURE_DIM*2,4,4) #resnet18　　resnet50裁剪版  resnet50filter裁剪版
+        relations = relation_network(relation_pairs).view(-1,CLASS_NUM)
+
+        mse = nn.MSELoss().cuda(GPU)
+        one_hot_labels = Variable(torch.zeros(BATCH_NUM_PER_CLASS*CLASS_NUM, CLASS_NUM).scatter_(1, batch_labels.view(-1,1), 1).cuda(GPU))
+        loss = mse(relations,one_hot_labels)
+
+
+        # training
+
+        feature_encoder.zero_grad()
+        relation_network.zero_grad()
+
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm(feature_encoder.parameters(),0.5)
+        torch.nn.utils.clip_grad_norm(relation_network.parameters(),0.5)
+
+        feature_encoder_optim.step()
+        relation_network_optim.step()
+
+
+        if (episode+1)%100 == 0:
+                # print("episode:",episode+1,"loss",loss.data[0])
+                logger.info("episode:"+str(episode+1) +" loss:"+ str(loss.data))
+
+        if episode%TEST_INTERVAL == 0: #5000
+
+            # test
+            logger.info("Testing...")
+            accuracies = []
+            for i in range(TEST_EPISODE):
+                total_rewards = 0
+                task = tg.MiniImagenetTask(metatest_folders,CLASS_NUM,SAMPLE_NUM_PER_CLASS,15)
+                sample_dataloader = tg.get_mini_imagenet_data_loader(task,num_per_class=SAMPLE_NUM_PER_CLASS,split="train",shuffle=False)
+
+                num_per_class = 5
+                test_dataloader = tg.get_mini_imagenet_data_loader(task,num_per_class=num_per_class,split="test",shuffle=False)
+
+                sample_images,sample_labels = sample_dataloader.__iter__().next()
+                for test_images,test_labels in test_dataloader:
+                    batch_size = test_labels.shape[0]
+                    # print('batch_size', batch_size,test_labels.shape)
+                    # calculate features
+                    sample_features = feature_encoder(Variable(sample_images).cuda(GPU)) # 5x64
+                    # sample_features = sample_features.view(CLASS_NUM,SAMPLE_NUM_PER_CLASS,FEATURE_DIM,19,19)
+                    # sample_features = sample_features.view(CLASS_NUM,SAMPLE_NUM_PER_CLASS,FEATURE_DIM,4,4) #resnet18　resnet50裁剪版 resnet50filter裁剪版
+                    sample_features = sample_features.view(CLASS_NUM,SAMPLE_NUM_PER_CLASS,FEATURE_DIM,8,8) #resnet50  #resnet50裁剪layer34+filter为256 (16,8,8)
+                    sample_features = torch.sum(sample_features,1).squeeze(1)
+                    test_features = feature_encoder(Variable(test_images).cuda(GPU)) # 20x64
+
+                    # calculate relations
+                    # each batch sample link to every samples to calculate relations
+                    # to form a 100x128 matrix for relation network
+                    sample_features_ext = sample_features.unsqueeze(0).repeat(batch_size,1,1,1,1)
+
+                    test_features_ext = test_features.unsqueeze(0).repeat(1*CLASS_NUM,1,1,1,1)
+                    test_features_ext = torch.transpose(test_features_ext,0,1)
+                    # relation_pairs = torch.cat((sample_features_ext,test_features_ext),2).view(-1,FEATURE_DIM*2,19,19)
+                    relation_pairs = torch.cat((sample_features_ext,test_features_ext),2).view(-1,FEATURE_DIM*2,8,8) #resnet50 #resnet50裁剪layer34+filter为256 (16,8,8)
+                    # relation_pairs = torch.cat((sample_features_ext,test_features_ext),2).view(-1,FEATURE_DIM*2,4,4)#resnet18　　resnet50裁剪版 resnet50filter裁剪版
+                    relations = relation_network(relation_pairs).view(-1,CLASS_NUM)
+
+                    _,predict_labels = torch.max(relations.data,1)
+
+                    rewards = [1 if predict_labels[j]==test_labels[j] else 0 for j in range(batch_size)]
+
+                    total_rewards += np.sum(rewards)
+
+
+                accuracy = total_rewards/1.0/CLASS_NUM/15
+                accuracies.append(accuracy)
+
+
+            test_accuracy,h = mean_confidence_interval(accuracies)
+
+            logger.info("test accuracy:"+str(test_accuracy) + " h:" +str(h))
+
+            if test_accuracy > last_accuracy:
+                # save networks
+                # torch.save(feature_encoder.state_dict(),str(args.output_dir + "miniimagenet_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl"))
+                # torch.save(feature_encoder.state_dict(),str(args.output_dir + "miniimagenet_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl"))
+                modelname = str("miniimagenet_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")
+                model_path = os.path.join(tracking.get_outputs_path(), modelname)
+                torch.save(feature_encoder.state_dict(),model_path)
+
+                modelname = str("miniimagenet_relation_network_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")
+                model_path = os.path.join(tracking.get_outputs_path(), modelname)
+                torch.save(relation_network.state_dict(),model_path)
+                
+                logger.info("save networks for episode: " + str(episode))
+
+                last_accuracy = test_accuracy
+
+
+
+#model = resnet50()
+
+if __name__ == '__main__':
+    main()
---
+++ omniglot/omniglot/parse.py
@@ -0,0 +1,206 @@
+import torch
+from torch import Tensor
+import torch.nn as nn
+from torch.nn import ReLU
+from utils import load_state_dict_from_url
+from typing import Type, Any, Callable, Union, List, Optional, Tuple
+from torch.nn.common_types import _size_1_t, _size_2_t, _size_3_t
+import torch.nn.modules.conv
+# from conv import Conv
+
+
+def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+# def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
+#     """3x3 convolution with padding"""
+#     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+#                      padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+# def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+#     """1x1 convolution"""
+#     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+def parse(in_planes: int, out_planes: int,  stride: int = 1) -> nn.Conv2d:
+    return Parse(in_planes, out_planes, stride=stride)
+
+
+
+
+
+#修改版
+class Parse(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        stride: _size_2_t = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super(Parse, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1 在跨度！= 1时，self.conv2和self.downsample层都会对输入进行下采样
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
+        #定义parse的各层
+
+        self.sigmoid = nn.Sigmoid()
+        self.conv1 = conv1x1(in_channels, 1, stride=1)
+        self.conv2 = conv1x1(in_channels, 1, stride=1)
+        self.conv3 = conv1x1(in_channels, 1, stride=1)
+
+        self.Relu = ReLU()
+
+        self.avgpooling1 = nn.AvgPool2d(kernel_size=8, stride=8, padding=0)
+        # tensorpack和pytorch的shape有区别,而且pytorch的3核卷积和二核卷积的参数写法不同（padding=1）所以写了以下两个，在forward函数里面定义调用哪个
+        self.conv2D1_3 = conv3x3(in_channels, 1*self.out_channels//4, stride=1)
+        self.conv2D1_1 = conv1x1(in_channels, 1*self.out_channels//4, stride=1)
+
+        self.avgpooling2 = nn.AvgPool2d(kernel_size=4, stride=4, padding=0)
+        self.conv2D2_3 = conv3x3(in_channels, 1*self.out_channels//4, stride=1)
+        self.conv2D2_1 = conv1x1(in_channels, 1*self.out_channels//4, stride=1)
+
+        self.avgpooling3 = nn.AvgPool2d(kernel_size=4, stride=4, padding=0)
+        self.conv2D3_3 = conv3x3(in_channels, 1*self.out_channels//4, stride=1)
+        self.conv2D3_1 = conv1x1(in_channels, 1*self.out_channels//4, stride=1)
+
+        self.conv2D5 = conv3x3(in_channels, 1*self.out_channels//4, stride=stride)
+
+        self.bn1 = norm_layer(self.in_channels//4)
+        self.bn2 = norm_layer(self.in_channels//4)
+        self.bn3 = norm_layer(self.in_channels//4)
+
+    #前向传播
+    def forward(self, l: Tensor) -> Tensor:
+        self.shape = l.shape
+        l_ori = l
+
+        l_ori = self.conv2D5(l_ori)
+        l_ori = self.Relu(l_ori)
+
+        parse1 = self.conv1(l)
+        parse1 = self.sigmoid(parse1)
+        l1_left = l * parse1
+        l = l - l1_left
+
+        parse2 = self.conv2(l)
+        parse2 = self.sigmoid(parse2)
+        l2_left = l * parse2
+        l2_right = l - l2_left
+
+        out_avgpooling1 = self.avgpooling1(l1_left)
+        if(self.shape[2]//8 > 2):
+            out_conv2D1 = self.conv2D1_3(out_avgpooling1)
+        else:
+            out_conv2D1 = self.conv2D1_1(out_avgpooling1)
+        # l1_left = tf.keras.backend.resize_images(out_conv2D1, 8//self.stride, 8//self.stride, 'channels_last' )
+        rs = Tuple[int,...]
+        # rs = [out_conv2D1.shape[2]*8//self.stride, out_conv2D1.shape[3]*8//self.stride]
+        rs = [l_ori.shape[2],l_ori.shape[3]]
+        l1_left = nn.functional.interpolate(out_conv2D1, size = rs, mode = 'nearest')
+
+        out_avgpooling2 = self.avgpooling2(l2_left)
+        # tensorpack和pytorch的shape有区别
+        if(self.shape[2]//4 > 2):
+            out_conv2D2 = self.conv2D2_3(out_avgpooling2)
+        else:
+            out_conv2D2 = self.conv2D2_1(out_avgpooling2)
+        # l2_left = tf.keras.backend.resize_images(out_conv2D2, 4//self.stride, 4//self.stride, 'channels_last')
+        ###!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        #找到了原因：l2是7 *4  l1是3*8!!!  原因是7Apooling之后，得到3.5，但是Pool是直接取整
+        l2_left = nn.functional.interpolate(out_conv2D2, size = rs, mode = 'nearest')
+
+        if(self.shape[2] > 2):
+            l2_right = self.conv2D3_3(l2_right)
+        else:
+            l2_right = self.conv2D3_1(l2_right)
+
+        bn1 = self.bn1(l1_left)
+        bn2 = self.bn2(l2_left)
+        bn3 = self.bn3(l2_right)
+
+        l = torch.cat([self.sigmoid(bn1) * l_ori, self.sigmoid(bn2) * l_ori,
+            self.sigmoid(bn3) * l_ori, self.sigmoid(bn3) * 0], 1)
+        return l
+
+
+    # #前向传播
+    # def forward(self, l: Tensor) -> Tensor:
+    #     # print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',l.shape)
+    #     self.shape = l.shape
+    #     l_ori = l
+
+    #     l_ori = self.conv2D5(l_ori)
+    #     l_ori = self.Relu(l_ori)
+
+    #     parse1 = self.conv1(l)
+    #     parse1 = self.sigmoid(parse1)
+    #     l1_left = l * parse1
+    #     l = l - l1_left
+
+    #     parse2 = self.conv2(l)
+    #     parse2 = self.sigmoid(parse2)
+    #     l2_left = l * parse2
+    #     l = l - l2_left
+
+    #     parse3 = self.conv3(l)
+    #     parse3 = self.sigmoid(parse3)
+    #     l3_left = l * parse3
+    #     l3_right = l - l3_left
+
+    #     out_avgpooling1 = self.avgpooling1(l1_left)
+    #     if(self.shape[2]//8 > 2):
+    #         out_conv2D1 = self.conv2D1_3(out_avgpooling1)
+    #     else:
+    #         out_conv2D1 = self.conv2D1_1(out_avgpooling1)
+    #     # l1_left = tf.keras.backend.resize_images(out_conv2D1, 8//self.stride, 8//self.stride, 'channels_last' )
+    #     rs = Tuple[int,...]
+    #     # rs = [out_conv2D1.shape[2]*8//self.stride, out_conv2D1.shape[3]*8//self.stride]
+    #     rs = [l_ori.shape[2],l_ori.shape[3]]
+    #     l1_left = nn.functional.interpolate(out_conv2D1, size = rs, mode = 'nearest')
+
+    #     out_avgpooling2 = self.avgpooling2(l2_left)
+    #     # tensorpack和pytorch的shape有区别
+    #     if(self.shape[2]//4 > 2):
+    #         out_conv2D2 = self.conv2D2_3(out_avgpooling2)
+    #     else:
+    #         out_conv2D2 = self.conv2D2_1(out_avgpooling2)
+    #     # l2_left = tf.keras.backend.resize_images(out_conv2D2, 4//self.stride, 4//self.stride, 'channels_last')
+    #     ###!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    #     #找到了原因：l2是7 *4  l1是3*8!!!  原因是7Apooling之后，得到3.5，但是Pool是直接取整
+        
+    #     l2_left = nn.functional.interpolate(out_conv2D2, size = rs, mode = 'nearest')
+
+    #     out_avgpooling3 = self.avgpooling3(l3_left)
+    #     if(self.shape[2]//2 > 2):
+    #         out_conv2D3 = self.conv2D3_3(out_avgpooling3)
+    #     else:
+    #         out_conv2D3 = self.conv2D3_1(out_avgpooling3)
+    #     # l3_left = tf.keras.backend.resize_images(out_conv2D3, 2//self.stride, 2//self.stride, 'channels_last')
+    #     l3_left = nn.functional.interpolate(out_conv2D3, size = rs, mode = 'nearest')
+
+    #     if(self.shape[2] > 2):
+    #         l3_right = self.conv2D4_3(l3_right)
+    #     else:
+    #         l3_right = self.conv2D4_1(l3_right)
+
+    #     bn1 = self.bn1(l1_left)
+    #     bn2 = self.bn2(l2_left)
+    #     bn3 = self.bn3(l3_left)
+    #     bn4 = self.bn4(l3_right)
+
+    #     l = torch.cat([self.sigmoid(bn1) * l_ori, self.sigmoid(bn2) * l_ori,
+    #         self.sigmoid(bn3) * l_ori, self.sigmoid(bn4) * l_ori], 1)
+    #     return l
+
